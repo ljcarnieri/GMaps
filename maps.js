42c0d215a6ebde716cb9043e457ffdda62792896
Vitor Villar <vitor.luis98@gmail.com>
2014-08-28 20:00:03 -0300

    AJustando o Maps.js

--- maps.js
+++ maps.js
@@ -3,81 +3,79 @@ function Map() {
 
 //Objeto mapa da lib maps.js
 Map.prototype.map = null;
-
-//Opções do mapa
 Map.prototype.map_options = null;
-
-//Array de markers
 Map.prototype.markers = new Array();
-
-//Array de polilynes
 Map.prototype.polylines = new Array();
-
-//Array de Rotas
 Map.prototype.routes = new Array();
-
-//Oopções do DirectionRenderer
-Map.prototype.direction_options = null;
-
-//Div onde o mapa será renderizado
 Map.prototype.div_map = null;
-
-//Objeto que é o centro do mapa
 Map.prototype.center = null;
-
-//Array que guarda os pontos a serem renderizados
 Map.prototype.points = null;
-
-//Zoom do mapa
 Map.prototype.zoom = 14;
-
-//Maximo de Zoom
 Map.prototype.max_zoom = 18;
+Map.prototype.mapTypeId = null;
+Map.prototype.streetViewControl = null;
 
-//----------------Metodos
-Map.prototype.setCenter = function(lat, long) {
+//Constants
+Map.prototype.ROADMAP = google.maps.MapTypeId.ROADMAP;
+Map.prototype.SATELLITE = google.maps.MapTypeId.SATELLITE;
+Map.prototype.HYBRID = google.maps.MapTypeId.HYBRID;
+Map.prototype.TERRAIN = google.maps.MapTypeId.TERRAIN;
+
+
+//Metodos
+Map.prototype.setCenter = function (lat, long) {
     this.center = new google.maps.LatLng(lat, long);
 };
 
-Map.prototype.setDiv = function(div_name) {
+Map.prototype.setDiv = function (div_name) {
     this.div_map = div_name;
 };
 
-Map.prototype.setPoints = function(points) {
-    this.points = points;
-};
-
-Map.prototype.setZoom = function(zoom) {
+Map.prototype.setZoom = function (zoom) {
     this.zoom = zoom;
 };
 
-Map.prototype.setMaxZoom = function(zoom) {
+Map.prototype.setMaxZoom = function (zoom) {
     this.max_zoom = zoom;
 };
 
-Map.prototype.getRoutes = function() {
+Map.prototype.setMapTypeId = function (mapTypeId) {
+    this.mapTypeId = mapTypeId;
+};
+Map.prototype.getMapTypeId = function () {
+    return this.mapTypeId;
+};
+Map.prototype.setStreetViewControl = function (streetViewControl) {
+    this.streetViewControl = streetViewControl;
+};
+
+Map.prototype.getStreetViewControl = function () {
+    return this.streetViewControl;
+};
+
+Map.prototype.getRoutes = function () {
     return this.routes;
 };
 
-Map.prototype.addRoute = function(route) {
+Map.prototype.addRoute = function (route) {
     if (route != null) {
         this.routes.push(route);
     }
 };
 
-Map.prototype.addMarker = function(marker) {
+Map.prototype.addMarker = function (marker) {
     if (marker != null) {
         this.markers.push(marker);
     }
 };
 
-Map.prototype.addPolyline = function(polyline) {
+Map.prototype.addPolyline = function (polyline) {
     if (polyline != null) {
         this.polylines.push(polyline);
     }
 };
 
-Map.prototype.removeRoutes = function() {
+Map.prototype.removeRoutes = function () {
     for (var i in this.routes) {
         if (this.routes[i] != null) {
             this.routes[i].clear();
@@ -86,7 +84,7 @@ Map.prototype.removeRoutes = function() {
     this.routes = [];
 };
 
-Map.prototype.removeMarkers = function() {
+Map.prototype.removeMarkers = function () {
     for (var i in this.markers) {
         if (this.markers[i] != null) {
             this.markers[i].clear();
@@ -95,7 +93,7 @@ Map.prototype.removeMarkers = function() {
     this.markers = [];
 };
 
-Map.prototype.removePolylines = function() {
+Map.prototype.removePolylines = function () {
     for (var i in this.polylines) {
         if (this.polylines[i] != null) {
             this.polylines[i].clear();
@@ -104,36 +102,36 @@ Map.prototype.removePolylines = function() {
     this.polylines = [];
 };
 
-Map.prototype.getMap = function() {
+Map.prototype.getMap = function () {
     return this.map;
 };
 
-Map.prototype.init = function() {
+Map.prototype.init = function () {
 
     //Seta as opções padroes do mapa
     this.map_options = {
         zoom: this.zoom,
         maxZoom: this.max_zoom,
         center: this.center,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        streetViewControl: false
+        mapTypeId: this.mapTypeId,
+        streetViewControl: this.streetViewControl
     };
 
     //Inicia o mapa
     this.map = new google.maps.Map(document.getElementById(this.div_map), this.map_options);
 };
 
-Map.prototype.removePolyline = function(index) {
+Map.prototype.removePolyline = function (index) {
     this.polylines[index].clear();
 };
 
-Map.prototype.changeZoom = function(zoom) {
+Map.prototype.changeZoom = function (zoom) {
     if (zoom != undefined && zoom > 0) {
         this.map.setZoom(zoom);
     }
 };
 
-Map.prototype.changeCenterPosition = function(pos) {
+Map.prototype.changeCenterPosition = function (pos) {
     if (pos != undefined && pos instanceof google.maps.LatLng) {
 
     }
